37799cfaa7a92cde97f86090a62991ac51b2384d
Yann Sionneau <yann@sionneau.net>
2020-03-05 23:29:00 +0100

    Add short script to do pnr in one command

--- blinky-soc/pnr.sh
+++ blinky-soc/pnr.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-/home/fallen/dev/nextpnr-xilinx/nextpnr-xilinx --chipdb /home/fallen/dev/nextpnr-xilinx/xilinx/xc7a35t.bin --xdc top.xdc --json top.json --write top_routed.json --fasm top.fasm
-source ~/dev/prjxray/utils/environment.sh
-/home/fallen/dev/prjxray/utils/fasm2frames.py --db-root /home/fallen/dev/prjxray/database/artix7 --part xc7a35tcsg324-1 top.fasm > top.frames
-/home/fallen/dev/prjxray/build/tools/xc7frames2bit --part_file /home/fallen/dev/prjxray/database/artix7/xc7a35tcsg324-1/part.yaml --part_name xc7a35tcsg324-1  --frm_file top.frames --output_file top.bit
+
+if [ -z "$PRJXRAY_DIR" ]; then
+	echo "You should set PRJXRAY_DIR env variable"
+	exit 1
+fi
+
+if [ -z "$NEXTPNR_DIR" ]; then
+	echo "You should set NEXTPNR_DIR env variable"
+	exit 1
+fi
+
+$NEXTPNR_DIR/nextpnr-xilinx --chipdb $NEXTPNR_DIR/nextpnr-xilinx/xilinx/xc7a35t.bin --xdc top.xdc --json top.json --write top_routed.json --fasm top.fasm
+source $PRJXRAY_DIR/utils/environment.sh
+$PRJXRAY_DIR/utils/fasm2frames.py --db-root $PRJXRAY_DIR/database/artix7 --part xc7a35tcsg324-1 top.fasm > top.frames
+$PRJXRAY_DIR/build/tools/xc7frames2bit --part_file $PRJXRAY_DIR/database/artix7/xc7a35tcsg324-1/part.yaml --part_name xc7a35tcsg324-1  --frm_file top.frames --output_file top.bit
